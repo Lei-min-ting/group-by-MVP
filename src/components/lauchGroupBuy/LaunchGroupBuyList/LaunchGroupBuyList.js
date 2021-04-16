40a2b3cadc7212bac67b0e32daa3583ce6a1ce75
Minting Lei <lmt19950609@gmail.com>
2021-04-16 19:57:37 -0400

    first edit on launch branch

---
+++ src/components/lauchGroupBuy/LaunchGroupBuyList/LaunchGroupBuyList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LaunchGroupBuyList = (props) => {
+    
+    return (
+        <div>
+            <ul>
+
+            </ul>  
+        </div>
+    )
+}
+
+export default LaunchGroupBuyList
