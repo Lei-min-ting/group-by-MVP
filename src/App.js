b1613fa3a4163e8e0c08a0c91b46b94f7cb37931
minting lei <lmt19950609@gmail.com>
2021-04-20 13:52:14 -0400

    this is second editing on app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ const App=()=> {
     <div>
       <LaunchGroupBuy/>
       <h1>hollow world, thhis is a test of github</h1>
+      <h1>hollow world2, thhis is a test2 of github</h1>
+      <h1>hollow world3, thhis is a test3 of github</h1>
+      <h1>hollow world4, thhis is a test4 of github</h1>
     </div>
   );
 }
